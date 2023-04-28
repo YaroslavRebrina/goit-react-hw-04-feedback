e0e18c124fa59e4e3586b2eb10ea984087a7f68d
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2023-04-28 10:15:42 +0300

    done

--- src/components/FeedbackWidget/FeedbackWidget.js
+++ src/components/FeedbackWidget/FeedbackWidget.js
@@ -1,54 +1,62 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { FeedbackOptions } from './FeedbackOptions';
 import { Statistics } from './Statistics';
 
 import css from './FeedbackWidget.module.css';
 
-export class FeedbackWidget extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const FeedbackWidget = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const stateNames = ['good', 'neutral', 'bad'];
+  const stateValues = [good, neutral, bad];
+
+  const handlerClick = e => {
+    switch (e.target.id) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+
+      default:
+        alert('Нема такої опції');
+    }
   };
 
-  handlerClick = e => {
-    this.setState({ [e.target.id]: this.state[e.target.id] + 1 });
-  };
-
-  countTotalFeedback = () => {
-    const stateValues = Object.values(this.state);
+  const countTotalFeedback = () => {
     return stateValues.reduce(
       (accamulator, currentValue) => accamulator + currentValue,
       0
     );
   };
 
-  countPositiveFeedbacks = () => {
-    const [good] = Object.values(this.state);
-
-    return `${((100 / this.countTotalFeedback()) * good).toFixed(2) || '0'}%`;
+  const countPositiveFeedbacks = () => {
+    return `${((100 / countTotalFeedback()) * good).toFixed(2) || '0'}%`;
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    return (
-      <div className={css.section__wrapper}>
-        <h1>Please leave feedback</h1>
-        <div className={css.buttons__wrapper}>
-          {Object.keys(this.state).map(item => (
-            <FeedbackOptions key={item} id={item} onClick={this.handlerClick} />
-          ))}
-        </div>
-        <h2>Statistics</h2>
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          countTotalFeedback={this.countTotalFeedback()}
-          countPositiveFeedback={this.countPositiveFeedbacks()}
-          message={'There is no feedback'}
-        />
+  return (
+    <div className={css.section__wrapper}>
+      <h1>Please leave feedback</h1>
+      <div className={css.buttons__wrapper}>
+        {stateNames.map(item => (
+          <FeedbackOptions key={item} id={item} onClick={handlerClick} />
+        ))}
       </div>
-    );
-  }
-}
+      <h2>Statistics</h2>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        countTotalFeedback={countTotalFeedback()}
+        countPositiveFeedback={countPositiveFeedbacks()}
+        message={'There is no feedback'}
+      />
+    </div>
+  );
+};
